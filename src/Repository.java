636450d5f5d6242cf4a6823d9ee5df919ac40bbb
kvn147 <kevin458377@gmail.com>
2024-05-30 18:58:26 -0700

    Finished Methods

--- src/Repository.java
+++ src/Repository.java
@@ -1,8 +1,6 @@
+import java.lang.*;
+import java.util.*;
 
-
-/**
- * TODO: Finish this class.
- */
 public class Repository {
 
     private String name;
@@ -10,8 +8,6 @@ public class Repository {
 
     // You may add more fields
 
-
-    //TODO Javadoc and finish
     /**
      * This method will initialize a new empty repository given a name  
      * @param name of repository
@@ -20,10 +16,10 @@ public class Repository {
         if (name == null) {
             throw new IllegalArgumentException("Not a Valid Repository Name");
         }
-        Commit<String> name = new LinkedList<>();
+        this.name = name;
+        this.head = null;
     }
-s
-    // TODO Javadoc and finish
+
     /**
      * This method will get the name of repository
      * @return name of repository
@@ -32,7 +28,6 @@ s
         return name;
     }
 
-    // TODO Javadoc and finish
     /**
      * This method will get the head of the repository
      * @return the head toString representation
@@ -41,26 +36,30 @@ s
         return head.message;
     }
 
-    // TODO Javadoc and finish
     /**
      * This method will get the size of the repository
      * @return the size of repository
      */
     public int getRepoSize(){
+        int size = 0;
+        Commit current = head;
 
-        return Commit.size;
+        while (current != null) {
+            size++;
+            current = current.past;
+        }
+        return size;
     }
 
-    // TODO Javadoc and finish
     /**
      * This method will determine if a target ID is in the repository
      * @param targetID looking for in the repository
      * @return true if found, otherwise false
      */
     public boolean contains(int targetID){
-
         Commit current = head;
-        for (int i = 0; i < size; i++) {
+
+        while(current != null) {
             if (current.id == targetID) {
                 return true;
             }
@@ -69,41 +68,44 @@ s
         return false;
     }
 
-    // TODO JavaDoc and finish
     /**
      * This method will create a new commit to the repository
      * @param message of the new commmit added
      * @return target ID of it in the repository
      */
     public String commit(String message){
-
-        return "";
+        head = new Commit(message, head);
+        return head.message;
     }
 
-    // TODO JavaDoc and finish
     /**
      * This method will remove a commit from the repository
      * @param targetID of the commit to be removed
      * @return true if successfully removed, otherwise false
      */
     public boolean drop(int targetID) {
-        
-        // check that the targetID exists in repo
-        if(contains(targetID) == true)) {
-            // continue
-            Commit current = head;
-            for (int i = 0; i < size; i++) {
-                current = current.past;
-            }
-        }
-        else {
+        // base case
+        if (head == null) {
             return false;
         }
-  
+        if (head.id == targetID) {
+            head = head.past;
+            return true;
+        }
+
+        // Linked List traversal
+        Commit current = head;
+        while (current.past != null) {
+            if (current.past.id == targetID) {
+                // pointer set to after target ID
+                current.past = current.past.past;
+                return true;
+            }
+            current = current.past;
+        }
+        return false;
     }
 
-
-    // TODO Javadoc and finish
     /**
      * This method will give the string representation of the 
      * most recent n commits of the repository
@@ -111,16 +113,30 @@ s
      * @return string representation of those commits
      */
     public String getHistory(int n){
+        String result = "";
+        Commit current = head;
+        int count = 0;
 
-        return "";
+        while (current != null && count < n) {
+            result += current.toString() + "\n";
+            current = current.past;
+            count++;
+        }
+        return result;
     }
 
-    // TODO Javadoc and finish
     /**
-     * This method will make the string representation
+     * This method will make the string representation <name> Current head: <head>
      */
     @Override
     public String toString(){
-       return "";
+       String result = name + "\n";
+
+       Commit current = head;
+       while (current != null) {
+           result += current.toString() + "\n";
+           current = current.past;
+       }
+       return result;
     }
 }
